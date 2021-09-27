6d4e9ce324d658dfc41359af3025d8b4a4ca8e61
alizada-hadi <alizadahadi2050@gmail.com>
2021-09-27 18:30:03 +0430

    ajax like

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,43 @@
+$(document).ready(() => {
+    $('.like-form').submit(function(e){ 
+        e.preventDefault()
+
+        const post_id = $(this).attr('id')
+        const likeText = $(`.like-btn${post_id}`).text()
+        const trim = $.trim(likeText)
+        const url = $(this).attr('action')
+        
+        let result
+        const likes = $(`.like-count${post_id}`).text()
+        const trimCount = parseInt(likes)
+
+        $.ajax({
+            type : "POST",
+            url : url,
+            data : {
+                'csrfmiddlewaretoken' : $('input[name=csrfmiddlewaretoken]').val(),
+                'post_id':post_id,
+            },
+            success : function(response){
+                if(trim === 'Unlike'){
+                    $(`.like-btn${post_id}`).text('Like')
+                    result = trimCount - 1
+                }
+                else{
+                    $(`.like-btn${post_id}`).text('Unlike')
+                    result = trimCount + 1
+                }
+                $(`.like-count${post_id}`).text(result)
+            },
+            error : function(error){
+                console.log(error);
+            }
+        })
+    })
+})
+
+
+
 const form = document.getElementById("comment")
 
 const comment = document.getElementById("id_comment")
@@ -35,3 +75,5 @@ form.addEventListener("submit", e => {
 
     })
 })
+
+
